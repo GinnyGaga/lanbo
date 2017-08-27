96195a2ec8bd8fd41f549b1014a3d7ca1cc48a05
mo <mo@example.com>
2017-08-27 23:39:32 +0800

    在程序中集成发送电子邮件功能

--- text.py
+++ text.py
@@ -3,8 +3,5 @@ from hello import mail
 msg=Message('test subject',sender='2269937513@qq.com',recipients=['2269937513@qq.com'])
 msg.body='text body'  
 msg.html='<b>HTML</b> body'   
-app_ctx=app.app_context()
-app_ctx.push()
-with app_ctx():
 with app.app_context():
 
