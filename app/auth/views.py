dee325b36685a219df7c156373a71b59ae3fe144
mo <mo@example.com>
2017-09-10 20:27:03 +0800

    8.5.1　添加用户注册表单

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,7 +1,9 @@
 from flask import render_template,redirect,request,url_for,flash
-from flask_login import login_user,logout_user,login_required
+from flask_login import login_user,logout_user,login_required,current_user
+from .. import db
 from ..models import User
-from .forms import LoginForm
+from ..email import send_email
+from .forms import LoginForm,RegistrationForm
 from . import auth
 
 @auth.route('/login',methods=['GET','POST'])
@@ -22,3 +24,15 @@ def logout():
 	logout_user()
 	flash('You have been logged out.')
 	return redirect(url_for('main.index'))
+
+@auth.route('/register',methods=['GET','POST'])
+def register():
+	form = RegistrationForm()
+	if form.validate_on_submit():
+		user=User(email=form.email.data,
+				username=form.username.data,
+				password=form.password.data)
+		db.session.add(user)
+		flash('You can now login.')
+		return redirect(url_for('auth.login'))
+	return render_template('auth/register.html',form=form)
