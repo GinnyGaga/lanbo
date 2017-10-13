ea4dc1f34df8ef1df477addc6d925f38c0b1e9e7
mo <mo@example.com>
2017-10-13 22:21:50 +0800

    10.4 用户头像

--- app/models.py
+++ app/models.py
@@ -3,8 +3,9 @@ from werkzeug.security import generate_password_hash,check_password_hash
 from flask_login import UserMixin,AnonymousUserMixin
 from . import login_manager
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
-from flask import current_app
+from flask import current_app,request
 from . import db
+import hashlib
 
 class Permission:
 	FOLLOW=0x01
@@ -145,6 +146,15 @@ class User(UserMixin,db.Model):
 		self.last_seen=datetime.utcnow()
 		db.session.add(self)
 		
+	def gravatar(self,size=100,default='identicon',rating='g'):
+		if request.is_secure:
+			url='https://secure.gravatar.com/avatar'
+		else:
+			url='http://www.gravatar.com/avatar'
+		hash = hashlib.md5(self.email.encode('utf-8')).hexdigest()
+		return '{url}/{hash}?s={size}&d={default}&r={rating}'.format(
+			url=url,hash=hash,size=size,default=default,rating=rating)
+
 	def __repr__(self):
 	 	return '<User %r>' % self.username
 
