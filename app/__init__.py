58297fb9966198da610fb71835e40c3f0607eeb9
mo <mo@example.com>
2017-10-16 08:01:49 +0800

    11.1　提交和显示博客文章

--- app/__init__.py
+++ app/__init__.py
@@ -29,7 +29,8 @@ def create_app(config_name):
 	app.register_blueprint(main_blueprint)
 
 	from .auth import auth as auth_blueprint
-	app.register_blueprint(auth_blueprint,url_prefix='/auth') #两个蓝本:main和auth注册到程序上。
+	app.register_blueprint(auth_blueprint,url_prefix='/auth') 
+
 
 
 	return app
--- app/models.py
+++ app/models.py
@@ -21,6 +21,7 @@ class Role(db.Model):
 	default=db.Column(db.Boolean,default=False,index=True)
 	permissions=db.Column(db.Integer)
 	users = db.relationship('User', backref='role', lazy='dynamic')
+
 	@staticmethod
 	def insert_roles():
 		roles={
@@ -61,7 +62,7 @@ class User(UserMixin,db.Model):
 	member_since = db.Column(db.DateTime(),default=datetime.utcnow)
 	last_seen = db.Column(db.DateTime(),default=datetime.utcnow)
 	avatar_hash = db.Column(db.String(32))
-
+	posts = db.relationship('Post',backref='author',lazy='dynamic')
 	
 	def __init__(self,**kwargs):
 		super(User,self).__init__(**kwargs)
@@ -177,4 +178,10 @@ def load_user(user_id):
 	return User.query.get(int(user_id))
 
 
+class Post(db.Model):
+	__tablename__= 'posts'
+	id = db.Column(db.Integer,primary_key=True)
+	body = db.Column(db.Text)
+	timestamp = db.Column(db.DateTime,index=True,default=datetime.utcnow)
+	author_id = db.Column(db.Integer,db.ForeignKey('users.id'))
 
